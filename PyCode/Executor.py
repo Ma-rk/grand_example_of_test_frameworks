b5ae62d02439ff354a561321b42d8b3686e2c53c
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-19 10:16:19 +0900

    메소드 추가: display_all_file_name()

--- PyCode/Executor.py
+++ PyCode/Executor.py
@@ -14,3 +14,8 @@ class Executor:
         for file_name in file_name_list:
             full_file_name = os.path.join(self.dir_path, file_name)
             self.file_full_name_list.append(full_file_name)
+
+    def display_all_file_name(self):
+        print('... display_all_file_name')
+        for file_name in self.file_full_name_list:
+            print(file_name)
