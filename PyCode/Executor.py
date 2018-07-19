dba78cc625b14e59bc158110e3f3d9ae8bd24d60
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-19 10:15:59 +0900

    메소드 추가: get_file_full_name_list()

--- PyCode/Executor.py
+++ PyCode/Executor.py
@@ -1,5 +1,16 @@
+import os
+
+
 class Executor:
     def __init__(self, dir_path: str):
         self.file_full_name_list = []
 
         self.dir_path = dir_path
+
+    def get_file_full_name_list(self):
+        print('... get_file_full_name_list')
+        file_name_list = os.listdir(self.dir_path)
+
+        for file_name in file_name_list:
+            full_file_name = os.path.join(self.dir_path, file_name)
+            self.file_full_name_list.append(full_file_name)
