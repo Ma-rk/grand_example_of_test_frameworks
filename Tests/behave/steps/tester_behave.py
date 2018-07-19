3bface49971a4c07611f7f13ba7a6b1bc316685c
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-19 14:24:03 +0900

    behave 시나리오 작성 및 구현: 커맨드라인 인수 전달.

--- Tests/behave/steps/tester_behave.py
+++ Tests/behave/steps/tester_behave.py
@@ -1,1 +1,18 @@
 from behave import *
+
+from PyCode.Executor import Executor
+
+
+@given('argument is "D Download installed" 인수는 "D Download installed"')
+def step_impl(context):
+    context.dir_path = 'D:\Download\installed'
+
+
+@When('pass "D Download installed"')
+def step_impl(context):
+    context.executor = Executor(context.dir_path)
+
+
+@Then('executor.dir_path is "D Download installed"')
+def step_impl(context):
+    assert context.executor.dir_path == context.dir_path
--- Tests/behave/tester.feature
+++ Tests/behave/tester.feature
@@ -1,3 +1,8 @@
 # Created by Marc at 2018-07-19
 Feature: tests Executor
   # Executor 를 테스트함.
+
+  Scenario: 커맨드라인 인수 전달 pass command line argument
+    Given argument is "D Download installed" 인수는 "D Download installed"
+    When pass "D Download installed"
+    Then executor.dir_path is "D Download installed"
