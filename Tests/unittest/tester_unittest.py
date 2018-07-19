66d64f9c60b47b202926a5a20cf27107ebd56782
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-19 10:30:33 +0900

    unittest 메소드 추가.

---
+++ Tests/unittest/tester_unittest.py
@@ -0,0 +1,24 @@
+import unittest
+
+from PyCode.Executor import Executor
+
+
+class TesterUnittest(unittest.TestCase):
+    def setUp(self):
+        self.executor = Executor('D:\Download\installed')
+        self.file_full_name_list = ['D:\Download\installed\Anaconda3-5.2.0-Windows-x86_64.exe'
+            , 'D:\Download\installed\BANDIZIP-SETUP-KR.EXE'
+            , 'D:\Download\installed\cmder_mini.zip'
+            , 'D:\Download\installed\DropboxInstaller.exe'
+            , 'D:\Download\installed\Git-2.18.0-64-bit.exe'
+            , 'D:\Download\installed\GitKrakenSetup.exe'
+            , 'D:\Download\installed\KakaoTalk_Setup.exe'
+            , 'D:\Download\installed\mRemoteNG-Portable-1.75.7012.16846.zip'
+            , 'D:\Download\installed\VSCodeSetup-x64-1.25.0.exe']
+
+    def test_get_file_full_name_list(self):
+        self.executor.get_file_full_name_list()
+        self.assertEqual(len(self.file_full_name_list), len(self.executor.file_full_name_list))
+
+        for i in range(len(self.file_full_name_list)):
+            self.assertEqual(self.file_full_name_list[i], self.executor.file_full_name_list[i])
