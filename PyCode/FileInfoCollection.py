17dac9e56a9cc14b3bacd5a9375099d42ab1e6cd
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-23 16:32:26 +0900

    메소드 추가: get_file_full_name_list()

--- PyCode/FileInfoCollection.py
+++ PyCode/FileInfoCollection.py
@@ -1,2 +1,16 @@
+import os
+
+
 class FileInfoCollection:
-    pass
+    def get_file_full_name_list(self, dir_path):
+        file_name_list = os.listdir(dir_path)
+        full_file_name_list = []
+
+        for file_name in file_name_list:
+            if not file_name.endswith('.py'):
+                continue
+
+            full_file_name = os.path.join(dir_path, file_name)
+            full_file_name_list.append(full_file_name)
+
+        return full_file_name_list
