8d80ed456dc2e4cef2987d57aa0bc8649664d699
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-24 11:18:04 +0900

    기능 변경: 리펙토링에 맞춰 unittest의 코드 업데이트.

--- Tests/unittest/tester_unittest.py
+++ Tests/unittest/tester_unittest.py
@@ -1,54 +1,35 @@
 import unittest
 
-from PyCode.Executor import Executor
+from PyCode.FileInfoCollection import FileInfoCollection
 from PyCode.FileInfoContainer import FileInfoContainer
 
 
 class TesterUnittest(unittest.TestCase):
-    def setUp(self):
-        self.executor = Executor('D:\Download\installed')
-        self.file_full_name_list = ['D:\Download\installed\Anaconda3-5.2.0-Windows-x86_64.exe'
-            , 'D:\Download\installed\BANDIZIP-SETUP-KR.EXE'
-            , 'D:\Download\installed\calc.feature'
-            , 'D:\Download\installed\calc.py'
-            , 'D:\Download\installed\cmder_mini.zip'
-            , 'D:\Download\installed\DropboxInstaller.exe'
-            , 'D:\Download\installed\Git-2.18.0-64-bit.exe'
-            , 'D:\Download\installed\GitKrakenSetup.exe'
-            , 'D:\Download\installed\HandPy.py'
-            , 'D:\Download\installed\KakaoTalk_Setup.exe'
-            , 'D:\Download\installed\Monoid-Regular.ttf'
-            , 'D:\Download\installed\Monoid-Retina.ttf'
-            , 'D:\Download\installed\mRemoteNG-Portable-1.75.7012.16846.zip'
-            , 'D:\Download\installed\py_calc_test.py'
-            , 'D:\Download\installed\Readme_License.html'
-            , 'D:\Download\installed\VSCodeSetup-x64-1.25.0.exe']
-        self.trimmed_list = ['D:\Download\installed\calc.py'
-            , 'D:\Download\installed\HandPy.py'
-            , 'D:\Download\installed\py_calc_test.py']
+    def test_container(self):
+        container = FileInfoContainer('D:\Download\installed\calc.py')
 
-    def test_get_file_full_name_list(self):
-        self.executor.get_file_full_name_list()
-        self.assertEqual(len(self.file_full_name_list), len(self.executor.file_full_name_list))
+        self.assertEqual(container.num_of_line, 26)
+        self.assertEqual(container.num_of_method, 4)
+        self.assertEqual(container.num_of_class, 2)
 
-        for i in range(len(self.file_full_name_list)):
-            self.assertEqual(self.file_full_name_list[i], self.executor.file_full_name_list[i])
+        container_info = container.get_info()
+        self.assertEqual(container_info,
+                         'file name: D:\Download\installed\calc.py\n\tline: 26\n\tmethod: 4\n\tclass: 2')
 
-    def test_remove_not_py_from_list(self):
-        self.executor.file_full_name_list = self.file_full_name_list
-        self.executor.remove_not_py_from_list()
+    def test_collection(self):
+        collection = FileInfoCollection('D:\Download\installed')
 
-        self.assertEqual(len(self.trimmed_list), len(self.executor.file_full_name_list))
+        container = collection.get(0)
+        self.assertEqual(container.num_of_line, 26)
+        self.assertEqual(container.num_of_method, 4)
+        self.assertEqual(container.num_of_class, 2)
 
-        for i in range(len(self.trimmed_list)):
-            self.assertEqual(self.trimmed_list[i], self.executor.file_full_name_list[i])
+        container = collection.get(1)
+        self.assertEqual(container.num_of_line, 7)
+        self.assertEqual(container.num_of_method, 0)
+        self.assertEqual(container.num_of_class, 0)
 
-    def test_initialize_file_info_container_instance(self):
-        container = FileInfoContainer('D:\Download\installed\py_calc_test.py')
+        container = collection.get(2)
         self.assertEqual(container.num_of_line, 10)
         self.assertEqual(container.num_of_method, 1)
         self.assertEqual(container.num_of_class, 1)
-
-    def test_get_info(self):
-        container = FileInfoContainer('D:\Download\installed\py_calc_test.py')
-        self.assertEqual('line: 10	method: 1	class: 1', container.get_info())
