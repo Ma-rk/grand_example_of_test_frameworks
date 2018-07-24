70f846444928c40842a1ba738397ee755df613b0
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-24 10:48:30 +0900

    메소드 추가: get()

--- PyCode/FileInfoCollection.py
+++ PyCode/FileInfoCollection.py
@@ -31,6 +31,14 @@ class FileInfoCollection:
 
         return file_info_container_list
 
+    def get(self, idx):
+        if idx < 0:
+            return None  # todo: 익셉션 IndexError 를 처리하도록 변경
+        if len(self.file_info_container_list) < idx:
+            return None  # todo: 익셉션 IndexError 를 처리하도록 변경
+
+        return self.file_info_container_list[idx]
+
     def display_collection_info(self):
         for file_info_container in self.file_info_container_list:
             print(file_info_container.get_info())
