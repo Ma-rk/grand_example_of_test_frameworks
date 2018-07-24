d88b5227fa4a1598770dde7ea8000e44e47fa701
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-24 11:06:04 +0900

    기능 변경: 리펙토링에 맞춰 behave의 코드 업데이트.

--- Tests/behave/steps/tester_behave.py
+++ Tests/behave/steps/tester_behave.py
@@ -1,121 +1,51 @@
-from behave import *
+from behave import given
+from behave import When
+from behave import Then
 
-from PyCode.Executor import Executor
+from PyCode.FileInfoCollection import FileInfoCollection
 from PyCode.FileInfoContainer import FileInfoContainer
 
 
-@given('argument is "D Download installed" 인수는 "D Download installed"')
+@given('파일명 D Download installed calc.py')
+def step_impl(context):
+    context.full_file_path = 'D:\Download\installed\calc.py'
+
+
+@When('D Download installed calc.py 로 FileInfoContainer 객체를 생성하면')
+def step_impl(context):
+    context.container = FileInfoContainer(context.full_file_path)
+
+
+@Then('line: 26 method: 4 class: 2')
+def step_impl(context):
+    assert context.container.num_of_line == 26
+    assert context.container.num_of_method == 4
+    assert context.container.num_of_class == 2
+
+
+@given('경로 D Download installed')
 def step_impl(context):
     context.dir_path = 'D:\Download\installed'
 
 
-@When('pass "D Download installed"')
+@When('D Download installed 로 FileInfoCollection 객체를 생성하면')
 def step_impl(context):
-    context.executor = Executor(context.dir_path)
+    context.collection = FileInfoCollection(context.dir_path)
 
 
-@Then('executor.dir_path is "D Download installed"')
+@Then('26/4/2 7/0/0 10/1/1')
 def step_impl(context):
-    assert context.executor.dir_path == context.dir_path
+    container = context.collection.get(0)
+    assert container.num_of_line == 26
+    assert container.num_of_method == 4
+    assert container.num_of_class == 2
 
+    container = context.collection.get(1)
+    assert container.num_of_line == 7
+    assert container.num_of_method == 0
+    assert container.num_of_class == 0
 
-@given('Executor 객체, 파일 목록 instance of Executor, list of files')
-def step_impl(context):
-    context.executor = Executor('D:\Download\installed')
-    context.file_full_name_list = ['D:\Download\installed\Anaconda3-5.2.0-Windows-x86_64.exe'
-        , 'D:\Download\installed\BANDIZIP-SETUP-KR.EXE'
-        , 'D:\Download\installed\calc.feature'
-        , 'D:\Download\installed\calc.py'
-        , 'D:\Download\installed\cmder_mini.zip'
-        , 'D:\Download\installed\DropboxInstaller.exe'
-        , 'D:\Download\installed\Git-2.18.0-64-bit.exe'
-        , 'D:\Download\installed\GitKrakenSetup.exe'
-        , 'D:\Download\installed\HandPy.py'
-        , 'D:\Download\installed\KakaoTalk_Setup.exe'
-        , 'D:\Download\installed\Monoid-Regular.ttf'
-        , 'D:\Download\installed\Monoid-Retina.ttf'
-        , 'D:\Download\installed\mRemoteNG-Portable-1.75.7012.16846.zip'
-        , 'D:\Download\installed\py_calc_test.py'
-        , 'D:\Download\installed\Readme_License.html'
-        , 'D:\Download\installed\VSCodeSetup-x64-1.25.0.exe']
-
-
-@When('파일목록 조회 read the list of files')
-def step_impl(context):
-    context.executor.get_file_full_name_list()
-
-
-@Then('주어진 목록과 조회한 목록이 일치 both of given list and actual list are equal')
-def step_impl(context):
-    given_list = context.file_full_name_list
-    actual_list = context.executor.file_full_name_list
-    assert given_list == actual_list
-
-
-@given('1. 파일목록, 2. .py가 아닌 항목 제거한 목록')
-def step_impl(context):
-    context.executor = Executor('D:\Download\installed')
-    context.executor.file_full_name_list = ['D:\Download\installed\Anaconda3-5.2.0-Windows-x86_64.exe'
-        , 'D:\Download\installed\BANDIZIP-SETUP-KR.EXE'
-        , 'D:\Download\installed\calc.feature'
-        , 'D:\Download\installed\calc.py'
-        , 'D:\Download\installed\cmder_mini.zip'
-        , 'D:\Download\installed\DropboxInstaller.exe'
-        , 'D:\Download\installed\Git-2.18.0-64-bit.exe'
-        , 'D:\Download\installed\GitKrakenSetup.exe'
-        , 'D:\Download\installed\HandPy.py'
-        , 'D:\Download\installed\KakaoTalk_Setup.exe'
-        , 'D:\Download\installed\Monoid-Regular.ttf'
-        , 'D:\Download\installed\Monoid-Retina.ttf'
-        , 'D:\Download\installed\mRemoteNG-Portable-1.75.7012.16846.zip'
-        , 'D:\Download\installed\py_calc_test.py'
-        , 'D:\Download\installed\Readme_License.html'
-        , 'D:\Download\installed\VSCodeSetup-x64-1.25.0.exe']
-    context.trimmed_list = ['D:\Download\installed\calc.py'
-        , 'D:\Download\installed\HandPy.py'
-        , 'D:\Download\installed\py_calc_test.py']
-
-
-@When('파일목록에서 .py가 아닌 항목을 제거했을 때')
-def step_impl(context):
-    context.executor.remove_not_py_from_list()
-
-
-@Then('작업한 목록과 주어진 목록이 일치')
-def step_impl(context):
-    given_list = context.trimmed_list
-    trimmed_list = context.executor.file_full_name_list
-    assert given_list == trimmed_list
-
-
-@given('대상파일 경로 D Download installed py_calc_test.py')
-def step_impl(context):
-    context.file_path = 'D:\Download\installed\py_calc_test.py'
-
-
-@When('FileInfoContainer 클래스를 D Download installed py_calc_test.py 파일로 초기화 했을 때')
-def step_impl(context):
-    context.container = FileInfoContainer('D:\Download\installed\py_calc_test.py')
-
-
-@Then('num_of_line: 9, num_of_method: 1, num_of_class: 0')
-def step_impl(context):
-    assert context.container.num_of_line == 10
-    assert context.container.num_of_method == 1
-    assert context.container.num_of_class == 1
-
-
-@given('대상파일 경로 D Download installed py_calc_test.py 2')
-def step_impl(context):
-    context.file_path = 'D:\Download\installed\py_calc_test.py'
-    
-
-@When('container 의 get_info 를 호출했을 때')
-def step_impl(context):
-    context.container = FileInfoContainer('D:\Download\installed\py_calc_test.py')
-    context.container_info = context.container.get_info()
-
-
-@Then('"line: 10	method: 1	class: 1" 를 반환함')
-def step_impl(context):
-    assert context.container_info == 'line: 10	method: 1	class: 1'
+    container = context.collection.get(2)
+    assert container.num_of_line == 10
+    assert container.num_of_method == 1
+    assert container.num_of_class == 1
--- Tests/behave/tester.feature
+++ Tests/behave/tester.feature
@@ -1,32 +1,14 @@
-# Created by Marc at 2018-07-19
-Feature: tests Executor
-  # Executor 를 테스트함.
+# Created by user at 2018-07-23
+Feature: tests FileInfoContainer and FileInfoCollection
+  # FileInfoContainer 와 FileInfoCollection 를 테스트함.
 
-  Scenario: 커맨드라인 인수 전달 pass command line argument
-    Given argument is "D Download installed" 인수는 "D Download installed"
-    When pass "D Download installed"
-    Then executor.dir_path is "D Download installed"
-
-
-  Scenario: 파일목록 작성하기
-    Given Executor 객체, 파일 목록 instance of Executor, list of files
-    When 파일목록 조회 read the list of files
-    Then 주어진 목록과 조회한 목록이 일치 both of given list and actual list are equal
+  Scenario: D:\Download\installed\calc.py 의 라인/메소드/클래스의 개수를 셈
+    Given 파일명 D Download installed calc.py
+    When D Download installed calc.py 로 FileInfoContainer 객체를 생성하면
+    Then line: 26 method: 4 class: 2
 
 
-  Scenario: 파일목록에서 .py가 아닌 항목 제거하기
-    Given 1. 파일목록, 2. .py가 아닌 항목 제거한 목록
-    When 파일목록에서 .py가 아닌 항목을 제거했을 때
-    Then 작업한 목록과 주어진 목록이 일치
-
-
-  Scenario: FileInfoContainer 객체 초기화 하기
-    Given 대상파일 경로 D Download installed py_calc_test.py
-    When FileInfoContainer 클래스를 D Download installed py_calc_test.py 파일로 초기화 했을 때
-    Then num_of_line: 9, num_of_method: 1, num_of_class: 0
-
-
-  Scenario: FileInfoContainer 객체의 정보 조회하기
-    Given 대상파일 경로 D Download installed py_calc_test.py 2
-    When container 의 get_info 를 호출했을 때
-    Then "line: 10	method: 1	class: 1" 를 반환함
+  Scenario: D:\Download\installed\의 *.py 의 라인/메소드/클래스의 개수를 셈
+    Given 경로 D Download installed
+    When D Download installed 로 FileInfoCollection 객체를 생성하면
+    Then 26/4/2 7/0/0 10/1/1
