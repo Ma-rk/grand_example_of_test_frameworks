243cc8928297ed804ecc8d864ec0ea4aad58fe61
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-26 09:44:57 +0900

    리팩토링 클래스: FileInfoCollection
    직접 파일을 읽는 구조에서 데이터를 외부에서 주입받는 방식으로 변경

--- PyCode/FileInfoCollection.py
+++ PyCode/FileInfoCollection.py
@@ -1,35 +1,6 @@
-import os
-
-from PyCode.FileInfoContainer import FileInfoContainer
-
-
 class FileInfoCollection:
-    def __init__(self, dir_path):
-
-        full_file_name_list = self._get_file_full_name_list(dir_path)
-        self.file_info_container_list = self._get_file_container_collection(full_file_name_list)
-
-    def _get_file_full_name_list(self, dir_path):
-        file_name_list = os.listdir(dir_path)
-        full_file_name_list = []
-
-        for file_name in file_name_list:
-            if not file_name.endswith('.py'):
-                continue
-
-            full_file_name = os.path.join(dir_path, file_name)
-            full_file_name_list.append(full_file_name)
-
-        return full_file_name_list
-
-    def _get_file_container_collection(self, full_file_name_list):
-        file_info_container_list = []
-
-        for full_file_name in full_file_name_list:
-            container = FileInfoContainer(full_file_name)
-            file_info_container_list.append(container)
-
-        return file_info_container_list
+    def __init__(self, file_info_container_list):
+        self.file_info_container_list = file_info_container_list
 
     def get(self, idx):
         if idx < 0:
