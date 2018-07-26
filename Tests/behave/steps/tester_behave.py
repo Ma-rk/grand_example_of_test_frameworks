ed0564cefe0d6a2c99ba802f0e3db4f43f012c6f
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-26 19:38:42 +0900

    기능 변경: behave에서 fixture 사용.

--- Tests/behave/steps/tester_behave.py
+++ Tests/behave/steps/tester_behave.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock
 
-from behave import given
+from behave import given, fixture, use_fixture
 from behave import When
 from behave import Then
 
@@ -56,6 +56,67 @@ def step_impl(context):
 
 @given('읽어온 폴더 내용')
 def step_impl(context):
+    use_fixture(data_provider, context)
+
+
+@When('읽어온 content_list 로 FileInfoCollection 객체를 생성하면')
+def step_impl(context):
+    container_list = Helper.generate_container_list(context.content_list)
+    context.collection = FileInfoCollection(container_list)
+
+
+@Then('2, 26, 4, 0, 7, 0, 1, 10, 1')
+def step_impl(context):
+    assert context.collection.get_container(-1) is None
+
+    container = context.collection.get_container(0)
+    assert container.file_name == 'mocked pass\calc.py'
+    assert container.num_of_class == 2
+    assert container.num_of_line == 26
+    assert container.num_of_method == 4
+
+    container = context.collection.get_container(1)
+    assert container.file_name == 'mocked pass\HandPy.py'
+    assert container.num_of_class == 0
+    assert container.num_of_line == 7
+    assert container.num_of_method == 0
+
+    container = context.collection.get_container(2)
+    assert container.file_name == 'mocked pass\py_calc_test.py'
+    assert container.num_of_class == 1
+    assert container.num_of_line == 10
+    assert container.num_of_method == 1
+
+    assert context.collection.get_container(3) is None
+
+
+@given('generate_container_list 를 모킹 for file')
+def step_impl(context):
+    use_fixture(data_provider, context)
+    content_list = context.content_list
+    Helper.generate_content_list_from_file = MagicMock(return_value=content_list)
+
+
+@When('모킹한 데이터로 FileInfoCollection 객체를 생성하면 for file')
+def step_impl(context):
+    context.collection = Helper.generate_collection('file', '')
+
+
+@given('generate_container_list 를 모킹 for db')
+def step_impl(context):
+    use_fixture(data_provider, context)
+    content_list = context.content_list
+    Helper.generate_content_list_from_db = MagicMock(return_value=content_list)
+
+
+@When('모킹한 데이터로 FileInfoCollection 객체를 생성하면 for db')
+def step_impl(context):
+    context.collection = Helper.generate_collection('db', '')
+
+
+@fixture
+def data_provider(context):
+    # setting up fixture
     context.content_list = [['mocked pass\\calc.py',
                              ['class Calc1:\n',
                               '    def add(self== num_a== num_b):\n',
@@ -104,148 +165,6 @@ def step_impl(context):
                               '\n']
                              ]
                             ]
-
-
-@When('읽어온 content_list 로 FileInfoCollection 객체를 생성하면')
-def step_impl(context):
-    container_list = Helper.generate_container_list(context.content_list)
-    context.collection = FileInfoCollection(container_list)
-
-
-@Then('2, 26, 4, 0, 7, 0, 1, 10, 1')
-def step_impl(context):
-    assert context.collection.get_container(-1) is None
-
-    container = context.collection.get_container(0)
-    assert container.file_name == 'mocked pass\calc.py'
-    assert container.num_of_class == 2
-    assert container.num_of_line == 26
-    assert container.num_of_method == 4
-
-    container = context.collection.get_container(1)
-    assert container.file_name == 'mocked pass\HandPy.py'
-    assert container.num_of_class == 0
-    assert container.num_of_line == 7
-    assert container.num_of_method == 0
-
-    container = context.collection.get_container(2)
-    assert container.file_name == 'mocked pass\py_calc_test.py'
-    assert container.num_of_class == 1
-    assert container.num_of_line == 10
-    assert container.num_of_method == 1
-
-    assert context.collection.get_container(3) is None
-
-
-@given('generate_container_list 를 모킹 for file')
-def step_impl(context):
-    Helper.generate_content_list_from_file = MagicMock(return_value=[['mocked pass\\calc.py',
-                                                                      ['class Calc1:\n',
-                                                                       '    def add(self== num_a== num_b):\n',
-                                                                       '        """\n',
-                                                                       '\n',
-                                                                       '        :param num_a: 1st no\n',
-                                                                       '        :param num_b: 2nd no\n',
-                                                                       '        :return: sum of two numbers\n',
-                                                                       '        """\n',
-                                                                       '        num_result = num_a+num_b\n',
-                                                                       '        return num_result\n',
-                                                                       '\n',
-                                                                       '    def subtract(self== num_a== num_b):\n',
-                                                                       '        num_result = num_a-num_b\n',
-                                                                       '        return num_result\n',
-                                                                       '\n',
-                                                                       '\n',
-                                                                       'class Calc2:\n',
-                                                                       '    def multiply(self== num_a== num_b):\n',
-                                                                       '        num_result = num_a*num_b\n',
-                                                                       '        return num_result\n',
-                                                                       '\n',
-                                                                       '    def divide(self== num_a== num_b):\n',
-                                                                       '        num_result = num_a/num_b\n',
-                                                                       '        return num_result\n',
-                                                                       '\n',
-                                                                       '\n']],
-                                                                     ['mocked pass\\HandPy.py',
-                                                                      ['import sys\n',
-                                                                       '\n',
-                                                                       'print(sys.executable)\n',
-                                                                       '\n',
-                                                                       'x = 1000\n',
-                                                                       'y = 1000\n',
-                                                                       'print(x is y)\n']],
-                                                                     ['mocked pass\\py_calc_test.py',
-                                                                      ['from calc import Calc1\n',
-                                                                       '\n',
-                                                                       '\n',
-                                                                       'def test_add():\n',
-                                                                       '    ut_calc1 = Calc1()\n',
-                                                                       '    assert ut_calc1.add(4== 6) == 10\n',
-                                                                       '\n',
-                                                                       'class YourClass:\n',
-                                                                       '    pass\n',
-                                                                       '\n']
-                                                                      ]
-                                                                     ])
-
-
-@When('모킹한 데이터로 FileInfoCollection 객체를 생성하면 for file')
-def step_impl(context):
-    context.collection = Helper.generate_collection('file', '')
-
-
-@given('generate_container_list 를 모킹 for db')
-def step_impl(context):
-    Helper.generate_content_list_from_db = MagicMock(return_value=[['mocked pass\\calc.py',
-                                                                    ['class Calc1:\n',
-                                                                     '    def add(self== num_a== num_b):\n',
-                                                                     '        """\n',
-                                                                     '\n',
-                                                                     '        :param num_a: 1st no\n',
-                                                                     '        :param num_b: 2nd no\n',
-                                                                     '        :return: sum of two numbers\n',
-                                                                     '        """\n',
-                                                                     '        num_result = num_a+num_b\n',
-                                                                     '        return num_result\n',
-                                                                     '\n',
-                                                                     '    def subtract(self== num_a== num_b):\n',
-                                                                     '        num_result = num_a-num_b\n',
-                                                                     '        return num_result\n',
-                                                                     '\n',
-                                                                     '\n',
-                                                                     'class Calc2:\n',
-                                                                     '    def multiply(self== num_a== num_b):\n',
-                                                                     '        num_result = num_a*num_b\n',
-                                                                     '        return num_result\n',
-                                                                     '\n',
-                                                                     '    def divide(self== num_a== num_b):\n',
-                                                                     '        num_result = num_a/num_b\n',
-                                                                     '        return num_result\n',
-                                                                     '\n',
-                                                                     '\n']],
-                                                                   ['mocked pass\\HandPy.py',
-                                                                    ['import sys\n',
-                                                                     '\n',
-                                                                     'print(sys.executable)\n',
-                                                                     '\n',
-                                                                     'x = 1000\n',
-                                                                     'y = 1000\n',
-                                                                     'print(x is y)\n']],
-                                                                   ['mocked pass\\py_calc_test.py',
-                                                                    ['from calc import Calc1\n',
-                                                                     '\n',
-                                                                     '\n',
-                                                                     'def test_add():\n',
-                                                                     '    ut_calc1 = Calc1()\n',
-                                                                     '    assert ut_calc1.add(4== 6) == 10\n',
-                                                                     '\n',
-                                                                     'class YourClass:\n',
-                                                                     '    pass\n',
-                                                                     '\n']
-                                                                    ]
-                                                                   ])
-
-
-@When('모킹한 데이터로 FileInfoCollection 객체를 생성하면 for db')
-def step_impl(context):
-    context.collection = Helper.generate_collection('db', '')
+    yield context.content_list
+    # cleaning up fixture
+    context.content_list = None
