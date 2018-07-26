6f560556d1ea3f079624c34e058e96b593a26870
Kim Hak Kyeol <k.mark.kr@gmail.com>
2018-07-26 15:41:49 +0900

    각종 코드 변경을 테스트코드에 반영.

--- Tests/pytest/tester_pytest.py
+++ Tests/pytest/tester_pytest.py
@@ -4,6 +4,63 @@ from PyCode.Helper import Helper
 
 
 class TestClass:
+    def setup_method(self):
+        self.org_generate_content_list_from_file = Helper.generate_content_list_from_file
+        self.org_generate_content_list_from_db = Helper.generate_content_list_from_db
+
+        self.mock_content_list = [['mocked pass\\calc.py',
+                                   ['class Calc1:\n',
+                                    '    def add(self== num_a== num_b):\n',
+                                    '        """\n',
+                                    '\n',
+                                    '        :param num_a: 1st no\n',
+                                    '        :param num_b: 2nd no\n',
+                                    '        :return: sum of two numbers\n',
+                                    '        """\n',
+                                    '        num_result = num_a+num_b\n',
+                                    '        return num_result\n',
+                                    '\n',
+                                    '    def subtract(self== num_a== num_b):\n',
+                                    '        num_result = num_a-num_b\n',
+                                    '        return num_result\n',
+                                    '\n',
+                                    '\n',
+                                    'class Calc2:\n',
+                                    '    def multiply(self== num_a== num_b):\n',
+                                    '        num_result = num_a*num_b\n',
+                                    '        return num_result\n',
+                                    '\n',
+                                    '    def divide(self== num_a== num_b):\n',
+                                    '        num_result = num_a/num_b\n',
+                                    '        return num_result\n',
+                                    '\n',
+                                    '\n']],
+                                  ['mocked pass\\HandPy.py',
+                                   ['import sys\n',
+                                    '\n',
+                                    'print(sys.executable)\n',
+                                    '\n',
+                                    'x = 1000\n',
+                                    'y = 1000\n',
+                                    'print(x is y)\n']],
+                                  ['mocked pass\\py_calc_test.py',
+                                   ['from calc import Calc1\n',
+                                    '\n',
+                                    '\n',
+                                    'def test_add():\n',
+                                    '    ut_calc1 = Calc1()\n',
+                                    '    assert ut_calc1.add(4== 6) == 10\n',
+                                    '\n',
+                                    'class YourClass:\n',
+                                    '    pass\n',
+                                    '\n']
+                                   ]
+                                  ]
+
+    def teardown_method(self):
+        Helper.generate_content_list_from_file = self.org_generate_content_list_from_file
+        Helper.generate_content_list_from_db = self.org_generate_content_list_from_db
+
     def test_generate_content_list(self):
         content_pair_list = Helper.generate_content_list_from_file('D:\Download\installed')
         for content_pair in content_pair_list:
@@ -37,8 +94,14 @@ class TestClass:
         assert container.num_of_line == 10
         assert container.num_of_method == 1
 
-    def test_generate_collection(self):
-        collection = Helper.generate_collection('file', 'D:\Download\installed')
+    def test_generate_collection_from_file(self):
+        self.help_generate_collection('file')
+
+    def test_generate_collection_from_db(self):
+        self.help_generate_collection('db')
+
+    def help_generate_collection(self, data_source: str):
+        collection = Helper.generate_collection(data_source, 'D:\Download\installed')
 
         assert collection.get_container(-1) is None
 
@@ -62,27 +125,19 @@ class TestClass:
 
         assert collection.get_container(3) is None
 
-    def test_generate_collection_with_mock(self):
-        mock_list = [['mocked pass\\calc.py',
-                      ['class Calc1:\n', '    def add(self, num_a, num_b:\n', '        """\n', '\n',
-                       '        :param num_a: 1st no\n', '        :param num_b: 2nd no\n',
-                       '        :return: sum of two numbers\n', '        """\n', '        num_result = num_a+num_b\n',
-                       '        return num_result\n', '\n', '    def subtract(self, num_a, num_b:\n',
-                       '        num_result = num_a-num_b\n', '        return num_result\n', '\n', '\n',
-                       'class Calc2:\n', '    def multiply(self, num_a, num_b:\n',
-                       '        num_result = num_a*num_b\n', '        return num_result\n', '\n',
-                       '    def divide(self, num_a, num_b:\n', '        num_result = num_a/num_b\n',
-                       '        return num_result\n', '\n', '\n']], ['mocked pass\\HandPy.py',
-                                                                     ['import sys\n', '\n', 'print(sys.executable\n',
-                                                                      '\n', 'x = 1000\n', 'y = 1000\n',
-                                                                      'print(x is y\n']],
-                     ['mocked pass\\py_calc_test.py',
-                      ['from calc import Calc1\n', '\n', '\n', 'def test_add(:\n', '    ut_calc1 = Calc1(\n',
-                       '    assert ut_calc1.add(4, 6 == 10\n', '\n', 'class YourClass:\n', '    pass\n', '\n']]]
+    def test_generate_collection_from_file_with_mock(self):
+        self.help_generate_collection_with_mock('file')
 
-        Helper.generate_content_list_from_file = MagicMock(return_value=mock_list)
+    def test_generate_collection_from_db_with_mock(self):
+        self.help_generate_collection_with_mock('db')
 
-        collection = Helper.generate_collection('file', 'mocked pass')
+    def help_generate_collection_with_mock(self, data_source: str):
+        if data_source == 'file':
+            Helper.generate_content_list_from_file = MagicMock(return_value=self.mock_content_list)
+        elif data_source == 'db':
+            Helper.generate_content_list_from_db = MagicMock(return_value=self.mock_content_list)
+
+        collection = Helper.generate_collection(data_source, 'mocked pass')
 
         assert collection.get_container(-1) is None
 
